daad498ad0d091af0b14bba5580beb6a64683e08
Galexanderg03 <galexanderg03@gmail.com>
2022-03-18 13:48:42 -0600

    Inicio

---
+++ src/lab9p2_juanoliva_gabrielgiron/Lab9P2_JuanOliva_GabrielGiron.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab9p2_juanoliva_gabrielgiron;
+
+/**
+ *
+ * @author Galex
+ */
+public class Lab9P2_JuanOliva_GabrielGiron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
